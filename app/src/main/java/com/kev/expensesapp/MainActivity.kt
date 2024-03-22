5c5c22408423aa3c04c1f81f7a42c47c14a5654b
Kevs <programadorweb.jr@skytex.com.mx>
2024-03-22 16:48:01 -0600

    estilo finalizado queda pendiente los inputs de agregar gasto cambiar colores iniciales

--- app/src/main/java/com/kev/expensesapp/MainActivity.kt
+++ app/src/main/java/com/kev/expensesapp/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.kev.expensesapp
 
+import android.app.Activity
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Window
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.kev.expensesapp.Adapters.ExpensesAdpater
@@ -13,20 +20,30 @@ import com.kev.expensesapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding
     private val layoutManager by lazy { LinearLayoutManager(this) }
+    private lateinit var dialog_expenses:Dialog
+    private lateinit var dialog_presupuesto:Dialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
         addData()
+        viewBinding.addGasto.setOnClickListener {
+            dialog_expenses.show()
+        }
+        viewBinding.addPresupuesto.setOnClickListener {
+            dialog_presupuesto.show()
+        }
     }
 
     private fun addData(){
-
+        dialog_expenses = initDialog(this,R.layout.dialog_agregar_gasto,0.90,0.45,true,true)
+        dialog_presupuesto = initDialog(this,R.layout.dialog_add_presupuesto,0.90,0.45,true,true)
         val recyclerView:RecyclerView = viewBinding.recyclerId
         recyclerView.layoutManager = layoutManager
         Log.e("Funcion add data","AddData")
         val data:ArrayList<expensesModel> = ArrayList()
-        data.add(expensesModel("gasto 1"," 20", "17/03/2024"))
+        //limitar a 100 caracteres la descripcion del gasto
+        data.add(expensesModel("Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha "," 20", "17/03/2024"))
         data.add(expensesModel("gasto 2"," 20", "17/03/2024"))
         data.add(expensesModel("gasto 3"," 20", "17/03/2024"))
         data.add(expensesModel("gasto 4"," 20", "17/03/2024"))
@@ -53,4 +70,27 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = costumerAdapter
 
     }
+
+
+     fun initDialog(
+        activity: Activity,
+        layout: Int,
+        ancho: Double,
+        alto: Double,
+        cancelable: Boolean,
+        transparente: Boolean
+    ): Dialog {
+        val dialog = Dialog(activity)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(cancelable)
+        dialog.setContentView(layout)
+        val width = (resources.displayMetrics.widthPixels * ancho).toInt()
+        val height = (resources.displayMetrics.heightPixels * alto).toInt()
+        dialog.window?.setLayout(width, height)
+        if (transparente)
+            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        return dialog
+    }
+
 }
